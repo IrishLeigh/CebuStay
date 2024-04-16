04f02357be14f1016a8a2452e318cfd00400393d
Rhadiel <112526625+Rhadiel@users.noreply.github.com>
2024-04-16 22:30:48 +0800

    Updates on UI (#2)
    Co-authored-by: shoyo <rhadielmari.escario@cit.edu>

--- fe/src/components/EditName.jsx
+++ fe/src/components/EditName.jsx
@@ -15,7 +15,7 @@ const EditName = () => {
   return (
     <div className="edit-name-container">
       <div className="edit-name-input-container">
-        <div className="edit-name-label">First Name</div>
+        <div className="edit-name-label" style={{ textAlign: 'left', marginLeft: '25px' }}>First Name</div>
         <div className="edit-name-input-wrapper">
           <MdPerson className="edit-name-icon" />
           <input
@@ -28,22 +28,21 @@ const EditName = () => {
         </div>
       </div>
       <div className="edit-name-input-container">
-        <div className="edit-name-label">Last Name</div>
+        <div className="edit-name-label" style={{ textAlign: 'left', marginLeft: '25px' }}>Last Name</div>
         <div className="edit-name-input-wrapper">
           <MdPerson className="edit-name-icon" />
           <input
             type="text"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
-            
             className="edit-name-input"
             placeholder="ESCARIO"
           />
         </div>
       </div>
       <div className="edit-name-buttons">
-        <button className="edit-button cancel-button">Cancel</button>
-        <button className="edit-button save-button" onClick={handleSave}>Save</button>
+        <button className="edit-button cancel-button" style={{ color: '#007bff', backgroundColor: 'white' }} onMouseEnter={(e) => { e.target.style.color = 'white'; e.target.style.backgroundColor = '#007bff' }} onMouseLeave={(e) => { e.target.style.color = '#007bff'; e.target.style.backgroundColor = 'white' }}>Cancel</button>
+        <button className="edit-button save-button" style={{ backgroundColor: '#007bff', color: 'white' }} onClick={handleSave}>Save</button>
       </div>
     </div>
   );
