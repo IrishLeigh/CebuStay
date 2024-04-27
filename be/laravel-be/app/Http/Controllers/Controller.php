9c33478108c618b55d5daef6a7cfe4e246d51430
idul <ludivicobalaterojr@gmail.com>
2024-04-27 22:51:06 +0800

    Controller used as Abstract class for enableCors function

--- be/laravel-be/app/Http/Controllers/Controller.php
+++ be/laravel-be/app/Http/Controllers/Controller.php
@@ -4,5 +4,20 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function enableCors($request)
+    {
+        $response = response()->json([], 200);
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Credentials', 'true');
+
+        // Access-Control headers are received during OPTIONS requests
+        if ($request->isMethod('options')) {
+            $response->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+            if ($request->header('Access-Control-Request-Headers')) {
+                $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
+            }
+            return $response;
+        }
+    }
 }
--- be/laravel-be/app/Http/Controllers/RegisterUserController.php
+++ be/laravel-be/app/Http/Controllers/RegisterUserController.php
@@ -12,22 +12,7 @@ use Carbon\Carbon;
 
 class RegisterUserController extends Controller
 {
-    public function enableCors($request)
-    {
-        $response = response()->json([], 200);
-        $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Credentials', 'true');
 
-        // Access-Control headers are received during OPTIONS requests
-        if ($request->isMethod('options')) {
-            $response->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-
-            if ($request->header('Access-Control-Request-Headers')) {
-                $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-            }
-            return $response;
-        }
-    }
     public function sendEmail($firstname, $lastname, $email, $verify_token)
     {
         $mail = new PHPMailer(true);
