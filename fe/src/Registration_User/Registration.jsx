31bec4462d526ee5389dbec3b54cca84fc5f5e72
shoyo <rhadielmari.escario@cit.edu>
2024-05-02 02:10:08 +0800

    Updates on Registration_User

--- fe/src/Registration_User/Registration.jsx
+++ fe/src/Registration_User/Registration.jsx
@@ -56,16 +56,16 @@ const Registration = () => {
       }
 
       // Continue with registration if all validations pass
-      // const account_type = "tourist";
-      // const is_verified = false;
-      // const currentDate = new Date();
-      // const year = currentDate.getFullYear();
-      // const month = String(currentDate.getMonth() + 1).padStart(2, "0");
-      // const day = String(currentDate.getDate()).padStart(2, "0");
-      // const hours = String(currentDate.getHours()).padStart(2, "0");
-      // const minutes = String(currentDate.getMinutes()).padStart(2, "0");
-      // const seconds = String(currentDate.getSeconds()).padStart(2, "0");
-      // const account_created = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+      const account_type = "tourist";
+      const is_verified = false;
+      const currentDate = new Date();
+      const year = currentDate.getFullYear();
+      const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+      const day = String(currentDate.getDate()).padStart(2, "0");
+      const hours = String(currentDate.getHours()).padStart(2, "0");
+      const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+      const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+      const account_created = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 
       // Send registration request
       const res = await axios.post("http://127.0.0.1:8000/api/registeruser", {
