1a74c79f52651712a9d0462698aa1d14f7cd90d2
ludivicoB <117965365+ludivicoB@users.noreply.github.com>
2024-05-22 18:31:57 +0800

    Added backend integration for View Property

--- fe/src/Properties_Listing/PropListing.js
+++ fe/src/Properties_Listing/PropListing.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 import Typography from "@mui/material/Typography";
@@ -11,6 +11,7 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Button from "@mui/material/Button";
 import ReservationForm from "./Reservation";
+import axios from "axios";
 
 function srcset(image, size, rows = 1, cols = 1) {
   return {
@@ -25,6 +26,56 @@ export default function QuiltedImageList() {
   const [checkInDate, setCheckInDate] = useState(null);
   const [checkOutDate, setCheckOutDate] = useState(null);
   const [showReservationForm, setShowReservationForm] = useState(false);
+
+  const [propertyImages, setPropertyImages] = useState([]);
+  const [propertyInfo, setPropertyInfo] = useState(null); // Initialize as null
+  const [loading, setLoading] = useState(true); // Loading state
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const propertyId = 60; // Replace with the ID of the property you want to fetch
+      try {
+        const res = await axios.get(
+          `http://127.0.0.1:8000/api/getfiles/${propertyId}`
+        );
+        if (res.data) {
+          // Transform the image data
+          const images = res.data.img.map((image, index) => ({
+            id: image.id,
+            src: image.src,
+            rows: index === 0 ? 2 : 1,
+            cols: index === 0 ? 2 : 1,
+          }));
+
+          // Set the transformed images to state
+          setPropertyImages(images);
+          console.log("PROPERTY IMAGES", images);
+          const res2 = await axios.get(
+            "http://127.0.0.1:8000/api/getproperty",
+            {
+              params: {
+                propertyid: propertyId,
+              },
+            }
+          );
+          if (res2.data) {
+            console.log("FULL PROPERTY INFO", res2.data);
+            setPropertyInfo(res2.data);
+            console.log(
+              "property name",
+              res2.data.property_details.property_name
+            );
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false); // Set loading to false after data is fetched
+      }
+    };
+    fetchData();
+  }, []);
+
   const [guestReview, setGuestReview] = useState({
     cleanliness: 0,
     location: 0,
@@ -63,128 +114,23 @@ export default function QuiltedImageList() {
     setShowReservationForm(true);
   };
 
-  const property_info = {
-    property_details: {
-      propertyid: 60,
-      property_name: "GardenBert",
-      property_desc: "Elahang Ludi sa Lacion",
-      property_type: "Home",
-      property_directions:
-        "Discover the vibrant hub of Megaworld Newtown Cebu, nestled in the heart of Mactan Island, easily accessible by taking a jeepney bound for Punta Engaño from downtown Cebu City. Start from downtown Cebu City near SM City Cebu or Ayala Center Cebu, common transportation hubs. Look for jeepneys heading to Punta Engaño or Lapu-Lapu City for Mactan Island access. Board a jeepney with appropriate signage or ask locals for confirmation. Pay the fare, typically PHP 40 to PHP 50, and ensure exact change if possible. Traverse either the Marcelo Fernan Bridge or Mactan-Mandaue Bridge to Mactan Island. Disembark at Punta Engaño in Lapu-Lapu City, the final stop. Megaworld Newtown Cebu is located nearby, adjacent to Mactan Newtown. Walk a short distance from the jeepney stop to reach specific destinations within the complex.",
-      unit_type: "Private room",
-    },
-    property_address: {
-      address: "Labangon",
-      zipcode: "6009",
-      latitude: "12312532",
-      longitude: "9823082098",
-    },
-    property_home: {
-      homeid: 26,
-      proppricingid: 10,
-      isoccupied: 0,
-    },
-    property_unitpricing: {
-      min_price: 1000,
-    },
-    property_unitrooms: {
-      unitid: 49,
-      guest_capacity: 3,
-      unitrooms: [
-        {
-          unitroomid: 177,
-          roomname: "Bedroom",
-          quantity: 2,
-        },
-        {
-          unitroomid: 178,
-          roomname: "Living Room",
-          quantity: 1,
-        },
-        {
-          unitroomid: 179,
-          roomname: "Bathroom",
-          quantity: 2,
-        },
-      ],
-      unitbeds: [
-        {
-          bedroomnum: "1",
-          beds: {
-            singlebed: 2,
-          },
-        },
-        {
-          bedroomnum: "2",
-          beds: {
-            largebed: 2,
-          },
-        },
-      ],
-    },
-    property_amenities: [
-      {
-        amenityid: 44,
-        amenity_name: "Toiletries",
-      },
-      {
-        amenityid: 45,
-        amenity_name: "Television",
-      },
-    ],
-    property_facilities: [
-      {
-        facilitiesid: 36,
-        facilities_name: "Gym",
-      },
-      {
-        facilitiesid: 37,
-        facilities_name: "Parking Area",
-      },
-      {
-        facilitiesid: 38,
-        facilities_name: "Swimming Pool",
-      },
-    ],
-    property_services: [
-      {
-        serviceid: 39,
-        service_name: "House Keeping",
-      },
-      {
-        serviceid: 40,
-        service_name: "Breakfast",
-      },
-      {
-        serviceid: 41,
-        service_name: "Pet Friendly",
-      },
-    ],
-    property_houserules: [
-      {
-        houserulesid: 21,
-        smoking_allowed: 1,
-        pets_allowed: 0,
-        parties_events_allowed: 1,
-        noise_restrictions: "No loud noises after 10 PM",
-        quiet_hours_start: "22:00:00",
-        quiet_hours_end: "07:00:00",
-        custom_rules: "No shoes inside the house.\nKeep the kitchen clean.",
-        check_in_from: "14:00:00",
-        check_in_until: "22:00:00",
-        check_out_from: "08:00:00",
-        check_out_until: "11:00:00",
-      },
-    ],
-    property_bookingpolicy: {
-      bookingpolicyid: 14,
-      is_cancel_plan: 1,
-      cancel_days: 7,
-      non_refundable: 1,
-      modification_plan: 1,
-      offer_discount: 0,
-    },
-  };
+  if (loading) {
+    return (
+      <Container maxWidth="xl" style={{ backgroundColor: "#F4F7FA" }}>
+        <CssBaseline />
+        <Typography variant="h4">Loading...</Typography>
+      </Container>
+    );
+  }
+
+  if (!propertyInfo) {
+    return (
+      <Container maxWidth="xl" style={{ backgroundColor: "#F4F7FA" }}>
+        <CssBaseline />
+        <Typography variant="h4">No property information available</Typography>
+      </Container>
+    );
+  }
 
   return (
     <>
@@ -196,7 +142,7 @@ export default function QuiltedImageList() {
             cols={4}
             rowHeight={Math.floor(window.innerHeight / 4)}
           >
-            {property_images.map((image) => (
+            {propertyImages.map((image) => (
               <ImageListItem
                 key={image.id}
                 cols={image.cols || 1}
@@ -258,12 +204,12 @@ export default function QuiltedImageList() {
                   fontFamily: "Poppins",
                 }}
               >
-                {property_info.property_details.property_name}
+                {propertyInfo.property_details.property_name}
               </Typography>
               <Typography
                 style={{ fontSize: "1.125rem", fontFamily: "Poppins" }}
               >
-                {property_info.property_details.property_desc}
+                {propertyInfo.property_details.property_desc}
               </Typography>
             </Paper>
             <Paper
@@ -289,7 +235,7 @@ export default function QuiltedImageList() {
                     Amenities
                   </Typography>
                   <div style={{ flexWrap: "wrap" }}>
-                    {property_info.property_amenities.map((amenity) => (
+                    {propertyInfo.property_amenities.map((amenity) => (
                       <div style={{ width: "50%" }} key={amenity.amenityid}>
                         <Typography>{amenity.amenity_name}</Typography>
                       </div>
@@ -307,7 +253,7 @@ export default function QuiltedImageList() {
                     Facilities
                   </Typography>
                   <div style={{ flexWrap: "wrap" }}>
-                    {property_info.property_facilities.map((facility) => (
+                    {propertyInfo.property_facilities.map((facility) => (
                       <div style={{ width: "50%" }} key={facility.facilitiesid}>
                         <Typography>{facility.facilities_name}</Typography>
                       </div>
@@ -325,7 +271,7 @@ export default function QuiltedImageList() {
                     Services
                   </Typography>
                   <div style={{ flexWrap: "wrap" }}>
-                    {property_info.property_services.map((service) => (
+                    {propertyInfo.property_services.map((service) => (
                       <div style={{ width: "50%" }} key={service.serviceid}>
                         <Typography>{service.service_name}</Typography>
                       </div>
@@ -438,7 +384,7 @@ export default function QuiltedImageList() {
               paddingLeft: "20px", // Add left padding to match bullet
             }}
           >
-            {property_info.property_details.property_directions}
+            {propertyInfo.property_details.property_directions}
           </Typography>
         </Paper>
         <ReservationForm />
@@ -446,36 +392,3 @@ export default function QuiltedImageList() {
     </>
   );
 }
-
-const property_images = [
-  {
-    id: 132,
-    src: "https://drive.google.com/thumbnail?id=1SvJFWaxbMkDnjvemhQWmr0VOGvZhbxb0",
-    rows: 2,
-    cols: 2,
-  },
-  {
-    id: 136,
-    src: "https://drive.google.com/thumbnail?id=1jtRFdO-jnsvOyvFh6tgW7vx5VlviK6oD",
-    rows: 1,
-    cols: 1,
-  },
-  {
-    id: 137,
-    src: "https://drive.google.com/thumbnail?id=1Ft6hiOwuPoTKyIz-xgSbGS_qfsFu1Fvd",
-    cols: 1,
-    rows: 1,
-  },
-  {
-    id: 138,
-    src: "https://drive.google.com/thumbnail?id=1SLTFennA-9Vcueawn9KCRSTw8kWsC12t",
-    cols: 1,
-    rows: 1,
-  },
-  {
-    id: 139,
-    src: "https://drive.google.com/thumbnail?id=1HXBjDcLXETGT4JzQuSN0wKtzel8nOvCk",
-    cols: 1,
-    rows: 1,
-  },
-];
