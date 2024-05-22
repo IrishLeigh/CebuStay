afcbefbd2da56489c5cca1860a924c1bb0741ad1
lRBRTlRobby <143729446+lRBRTlRobby@users.noreply.github.com>
2024-05-23 03:04:24 +0800

    Booking details (#10)
    * Updated the UI and put logic
    
    * BookingPage post request with fetchdata of property
    
    * Booking Detail and Payment
    
    * Updated the UI and put logic
    
    * BookingPage post request with fetchdata of property
    
    * Booking Detail and Payment
    
    ---------
    
    Co-authored-by: IrishLeigh <143740109+IrishLeigh@users.noreply.github.com>
    Co-authored-by: ludivicoB <117965365+ludivicoB@users.noreply.github.com>

--- fe/src/index.js
+++ fe/src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client"; // Updated import
 import App from "./App";
+import BookingDetails from "./pages/Booking/BookingDetails";
+import BookingPage from "./pages/Booking/BookingPage";
 
 const link = document.createElement('link');
 link.rel = 'stylesheet';
@@ -11,6 +13,6 @@ document.head.appendChild(link);
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <BookingPage />
   </BrowserRouter>
 );
