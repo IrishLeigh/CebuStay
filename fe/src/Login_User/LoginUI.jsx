c4f41ca27347bcb2c7b4d3a47f43fee4a22aa5dc
Kaye <kayegdp@gmail.com>
2024-06-24 18:51:45 +0800

    Update LoginUI Button Loader

--- fe/src/Login_User/LoginUI.jsx
+++ fe/src/Login_User/LoginUI.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import './Form.css';
-import { Link, useNavigate } from 'react-router-dom';
+import "./Form.css";
+import { Link, useNavigate } from "react-router-dom";
 import { MdVisibility, MdVisibilityOff } from "react-icons/md";
 import { useUser } from "../components/UserProvider";
 
-
-const LoginUI = ({setToken}) => {
+const LoginUI = ({ setToken }) => {
   const { loginUser } = useUser();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -15,6 +14,9 @@ const LoginUI = ({setToken}) => {
   const [rememberMe, setRememberMe] = useState(false);
   const [emptyEmail, setEmptyEmail] = useState(false);
   const [emptyPassword, setEmptyPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [snackbarVisible, setSnackbarVisible] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
   const [user, setUser] = useState([]);
   const navigate = useNavigate(); // Use useNavigate for navigation
 
@@ -53,7 +55,7 @@ const LoginUI = ({setToken}) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    
+
     if (!email && !password) {
       setEmptyEmail(true);
       setEmptyPassword(true);
@@ -80,8 +82,11 @@ const LoginUI = ({setToken}) => {
         setLoginError("");
       }, 3000);
       return;
- 
     }
+
+    setLoading(true); // Start loading
+    setLoginError("");
+    setSnackbarVisible(false);
 
     try {
       const response = await axios.post("http://127.0.0.1:8000/api/login", {
@@ -92,7 +97,7 @@ const LoginUI = ({setToken}) => {
         const token = response.data["token"];
         localStorage.setItem("auth_token", token);
         setToken(token);
-  
+
         if (rememberMe) {
           localStorage.setItem("remembered_email", email);
           localStorage.setItem("remembered_password", password);
@@ -101,8 +106,10 @@ const LoginUI = ({setToken}) => {
           localStorage.removeItem("remembered_password");
         }
 
-        console.log("Login successful! User iS: ", response.data);
-        console.log(response.data["message"]);
+        // console.log("Login successful! User iS: ", response.data);
+        // console.log(response.data["message"]);
+        setSnackbarMessage("Login successful!");
+        setSnackbarVisible(true);
         // console.log("Login successful!");
 
         navigate("/landing"); // Correct usage of navigate function
@@ -112,7 +119,10 @@ const LoginUI = ({setToken}) => {
         console.log("Login failed");
       }
     } catch (error) {
-      console.error("Error logging in:", error);
+      // console.error("Error logging in:", error);
+      setLoginError("Login failed. Please try again.");
+    } finally {
+      setLoading(false);
     }
 
     // Clear the login error after 2 seconds
@@ -120,7 +130,7 @@ const LoginUI = ({setToken}) => {
       setLoginError("");
     }, 3000);
   };
-  
+
   useEffect(() => {
     // Check if there are remembered credentials in local storage and fill in the fields
     const rememberedEmail = localStorage.getItem("remembered_email");
@@ -138,19 +148,56 @@ const LoginUI = ({setToken}) => {
     }
   }, []);
 
+  const handleSnackbarClose = () => {
+    setSnackbarVisible(false);
+  };
+
+  const buttonStyle = {
+    background: "#1780CB",
+    position: "relative",
+    padding: "10px 20px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "50px",
+    width: "350px", // Adjust width to your preference
+    cursor: "pointer",
+    border: "none",
+    color: "white",
+    fontSize: "16px",
+  };
+
+  const loaderStyle = {
+    border: "4px solid rgb(178, 190, 181)",
+    borderTopColor: "transparent",
+    borderRadius: "50%",
+    width: "20px",
+    height: "20px",
+    animation: loading ? "spin 1s linear infinite" : "none",
+  };
+
   return (
     <div className="formContainer">
       <div className="form-container">
-        <div style={{ textAlign: 'center' }}>
-          <h2 style={{ fontWeight: '600', fontFamily: 'Open Sans' }}>Login to your account</h2>
+        <div style={{ textAlign: "center" }}>
+          <h2 style={{ fontWeight: "600", fontFamily: "Open Sans" }}>
+            Login to your account
+          </h2>
         </div>
         <form className="form">
           {/* Email Input */}
           <div className="email">
-            <label style={{ marginRight: '85%', fontWeight: '10px' }}>Email</label>
+            <label style={{ marginRight: "85%", fontWeight: "10px" }}>
+              Email
+            </label>
           </div>
           <div className="inputForm">
-            <svg height="20" viewBox="0 0 32 32" width="20" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              height="20"
+              viewBox="0 0 32 32"
+              width="20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <g id="Layer_3" data-name="Layer 3">
                 <path d="m30.853 13.87a15 15 0 0 0 -29.729 4.082 15.1 15.1 0 0 0 12.876 12.918 15.6 15.6 0 0 0 2.016.13 14.85 14.85 0 0 0 7.715-2.145 1 1 0 1 0 -1.031-1.711 13.007 13.007 0 1 1 5.458-6.529 2.149 2.149 0 0 1 -4.158-.759v-10.856a1 1 0 0 0 -2 0v1.726a8 8 0 1 0 .2 10.325 4.135 4.135 0 0 0 7.83.274 15.2 15.2 0 0 0 .823-7.455zm-14.853 8.13a6 6 0 1 1 6-6 6.006 6.006 0 0 1 -6 6z"></path>
               </g>
@@ -163,13 +210,19 @@ const LoginUI = ({setToken}) => {
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-
           {/* Password Input */}
           <div className="password">
-            <label style={{ marginRight: '79%', fontWeight: '20px' }}>Password</label>
+            <label style={{ marginRight: "79%", fontWeight: "20px" }}>
+              Password
+            </label>
           </div>
           <div className="inputForm">
-            <svg height="20" viewBox="-64 0 512 512" width="20" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              height="20"
+              viewBox="-64 0 512 512"
+              width="20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path d="M336 512h-288c-26.453125 0-48-21.523438-48-48v-224c0-26.476562 21.546875-48 48-48h288c26.453125 0 48 21.523438 48 48v224c0 26.476562-21.546875 48-48 48zm-288-288c-8.8125 0-16 7.167969-16 16v224c0 8.832031 7.1875 16 16 16h288c8.8125 0 16-7.167969 16-16v-224c0-8.832031-7.1875-16-16-16zm0 0"></path>
               <path d="M304 224c-8.832031 0-16-7.167969-16-16v-80c0-52.929688-43.070312-96-96-96s-96 43.070312-96 96v80c0 8.832031-7.167969 16-16 16s-16-7.167969-16-16v-80c0-70.59375 57.40625-128 128-128s128 57.40625 128 128v80c0 8.832031-7.167969 16-16 16zm0 0"></path>
             </svg>
@@ -182,19 +235,31 @@ const LoginUI = ({setToken}) => {
               style={{ paddingRight: "30px" }}
             />
             {passwordVisibility ? (
-              <MdVisibility onClick={togglePasswordVisibility} style={{ paddingRight: "30px" }} />
+              <MdVisibility
+                onClick={togglePasswordVisibility}
+                style={{ paddingRight: "30px" }}
+              />
             ) : (
-              <MdVisibilityOff onClick={togglePasswordVisibility} style={{ paddingRight: "30px" }} />
+              <MdVisibilityOff
+                onClick={togglePasswordVisibility}
+                style={{ paddingRight: "30px" }}
+              />
             )}
           </div>
-
-          <div style={{ color: 'red', textAlign: 'center' }}>
-            {(emptyEmail || emptyPassword) && "Please fill in all the required fields"}
+          <div style={{ color: "red", textAlign: "center" }}>
+            {(emptyEmail || emptyPassword) &&
+              "Please fill in all the required fields"}
             {loginError && !emptyEmail && !emptyPassword && loginError}
           </div>
-
-          <div className="remember-forgot" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-            <div className="remember" style={{ textAlign: 'left' }}>
+          <div
+            className="remember-forgot"
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <div className="remember" style={{ textAlign: "left" }}>
               <input
                 type="checkbox"
                 checked={rememberMe}
@@ -203,25 +268,28 @@ const LoginUI = ({setToken}) => {
               <label>Remember me</label>
             </div>
 
-            <div className="forgotpassword" style={{ textAlign: 'right' }}>
+            <div className="forgotpassword" style={{ textAlign: "right" }}>
               <Link to="/login/forgot-password">
                 <span className="span">Forgot password?</span>
               </Link>
             </div>
           </div>
 
-          <button className="button-submit" style={{ background: "#1780CB" }} onClick={handleSubmit}>Login to Continue</button>
+          <button style={buttonStyle} onClick={handleSubmit} disabled={loading}>
+            {loading ? <div style={loaderStyle}></div> : "Login to Continue"}
+          </button>
 
-          <p className="p">Don't have an account?
-            <Link to="/register" className="span">Sign Up</Link>
+          <p className="p">
+            Don't have an account?
+            <Link to="/register" className="span">
+              Sign Up
+            </Link>
           </p>
-
           <p className="p line">Or With</p>
-
           {/* Google Sign In Button */}
           <div className="flex-row">
             <button className="btn google">
-            <svg
+              <svg
                 version="1.1"
                 width="20"
                 id="Layer_1"
@@ -233,19 +301,19 @@ const LoginUI = ({setToken}) => {
                 xmlSpace="preserve"
               >
                 <path
-                  style={{ fill: '#FBBB00' }}
+                  style={{ fill: "#FBBB00" }}
                   d="M113.47,309.408L95.648,375.94l-65.139,1.378C11.042,341.211,0,299.9,0,256 c0-42.451,10.324-82.483,28.624-117.732h0.014l57.992,10.632l25.404,57.644c-5.317,15.501-8.215,32.141-8.215,49.456 C103.821,274.792,107.225,292.797,113.47,309.408z"
                 ></path>
                 <path
-                  style={{ fill: '#518EF8' }}
+                  style={{ fill: "#518EF8" }}
                   d="M507.527,208.176C510.467,223.662,512,239.655,512,256c0,18.328-1.927,36.206-5.598,53.451 c-12.462,58.683-45.025,109.925-90.134,146.187l-0.014-0.014l-73.044-3.727l-10.338-64.535 c29.932-17.554,53.324-45.025,65.646-77.911h-136.89V208.176h138.887L507.527,208.176L507.527,208.176z"
                 ></path>
                 <path
-                  style={{ fill: '#28B446' }}
+                  style={{ fill: "#28B446" }}
                   d="M416.253,455.624l0.014,0.014C372.396,490.901,316.666,512,256,512 c-97.491,0-182.252-54.491-225.491-134.681l82.961-67.91c21.619,57.698,77.278,98.771,142.53,98.771 c28.047,0,54.323-7.582,76.87-20.818L416.253,455.624z"
                 ></path>
                 <path
-                  style={{ fill: '#F14336' }}
+                  style={{ fill: "#F14336" }}
                   d="M419.404,58.936l-82.933,67.896c-23.335-14.586-50.919-23.012-80.471-23.012 c-66.729,0-123.429,42.957-143.965,102.724l-83.397-68.276h-0.014C71.23,56.123,157.06,0,256,0 C318.115,0,375.068,22.126,419.404,58.936z"
                 ></path>
               </svg>
@@ -286,8 +354,6 @@ const LoginUI = ({setToken}) => {
             </button> */}
           </div>
         </form>
-
-        
       </div>
     </div>
   );
