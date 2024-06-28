54c6afb750e32cb79e7da357b415236c37f0121d
shoyo <rhadielmari.escario@cit.edu>
2024-06-28 22:03:20 +0800

    Search filter update

--- fe/src/pages/SearchAndFilter/components/Search.jsx
+++ fe/src/pages/SearchAndFilter/components/Search.jsx
@@ -1,3 +1,4 @@
+// Search.jsx
 import React, { useState } from 'react';
 import './Search.css';
 import { MdSearch } from 'react-icons/md';
@@ -10,18 +11,13 @@ export default function Search({ onSearch }) {
     const [guestCapacity, setGuestCapacity] = useState(''); // State for guest capacity
 
     const handleSearch = () => {
-        // Call the onSearch prop function with search parameters
-        onSearch({ 
-            startDate: startDate.toISOString().split('T')[0], // Format dates as strings
-            endDate: endDate ? endDate.toISOString().split('T')[0] : null, 
-            guestCapacity: guestCapacity ? parseInt(guestCapacity, 10) : null 
-        });
+        // Pass null for guestCapacity if the input is empty
+        onSearch({ startDate, endDate, guestCapacity: guestCapacity || null });
     };
-
     return (
-        <div>
-            <div className="max-w-4xl mx-auto p-4">
-                <div className="search-box">
+        <div >
+            <div className="max-w-4xl mx-auto p-4" style={{width:'71rem',marginTop:'2rem'}}>
+                <div className="search-box" >
                     <input type="text" placeholder="Search Destination" className="input-field" id="whereInput" />
                     {/* Datepicker for "From" */}
                     <DatePicker
@@ -53,8 +49,8 @@ export default function Search({ onSearch }) {
                         id="guestInput"
                         value={guestCapacity}
                         onChange={e => setGuestCapacity(e.target.value)}
-                    />
-                    <button className="search-button" id="searchButton" onClick={handleSearch}>
+                   />
+                    <button className="search-button" id="searchButton" onClick={handleSearch} style={{height:'3rem'}}>
                         <MdSearch className="search-icon" />
                     </button>
                 </div>
