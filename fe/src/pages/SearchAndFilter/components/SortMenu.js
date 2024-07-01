c0c6a709e6999ec43afae4a91c14bef3e9d1509c
shoyo <rhadielmari.escario@cit.edu>
2024-07-01 23:24:20 +0800

    added a margin

--- fe/src/pages/SearchAndFilter/components/SortMenu.js
+++ fe/src/pages/SearchAndFilter/components/SortMenu.js
@@ -4,7 +4,7 @@ import "../css/SortMenu.css"; // Import your CSS file
 
 export default function SortMenu() {
   return (
-    <Box className="sort-menu">
+    <Box className="sort-menu" sx={{ marginBottom: '1rem' }}>
       <button className="sort-btn">Sort</button>
       <button className="sort-btn">Best Match</button>
       <button className="sort-btn">Top reviewed</button>
