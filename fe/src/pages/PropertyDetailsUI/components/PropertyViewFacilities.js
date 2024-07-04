2ce1d53064501499172c77cd8886904a775f27a6
Kaye <kayegdp@gmail.com>
2024-07-04 20:59:03 +0800

    Edited Components (Cancellation, HouseRules, Amenities, Services, Facilities, PropertyBenefits.css) on PropertyDetailsUI

--- fe/src/pages/PropertyDetailsUI/components/PropertyViewFacilities.js
+++ fe/src/pages/PropertyDetailsUI/components/PropertyViewFacilities.js
@@ -33,16 +33,20 @@ const Facilities = ({ facilities = [] }) => (
     </div>
     <Divider sx={{ width: "100%", color: "#ccc" }} />
     <div className="amenity-cntr">
-      {facilities.map((facility) => (
-        <div className="each-amenity" key={facility.facilities_name}>
-          <img
-            src={facilitiesIcons[facility.facilities_name]}
-            alt={facility.facilities_name}
-            style={{ width: "24px", height: "24px", marginRight: "8px" }}
-          />
-          <div className="rooms-name">{facility.facilities_name}</div>
-        </div>
-      ))}
+      {facilities.length > 0 ? (
+        facilities.map((facility) => (
+          <div className="each-amenity" key={facility.facilities_name}>
+            <img
+              src={facilitiesIcons[facility.facilities_name]}
+              alt={facility.facilities_name}
+              style={{ width: "24px", height: "24px", marginRight: "8px" }}
+            />
+            <div className="rooms-name">{facility.facilities_name}</div>
+          </div>
+        ))
+      ) : (
+        <div className="no-facilities">No Facilities Available</div>
+      )}
     </div>
   </Paper>
 );
