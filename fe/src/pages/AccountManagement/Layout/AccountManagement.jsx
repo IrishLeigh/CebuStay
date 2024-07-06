99a23c3f90b5f83987a2ece8e20aa107dc2d761d
IrishLeigh <143740109+IrishLeigh@users.noreply.github.com>
2024-07-07 01:25:02 +0800

    Updated the logic with API for account ID

--- fe/src/pages/AccountManagement/Layout/AccountManagement.jsx
+++ fe/src/pages/AccountManagement/Layout/AccountManagement.jsx
@@ -91,15 +91,62 @@
 // };
 
 
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
 import AccountSidebar from '../components/AccountSidebar';
 import BookingHistory from '../components/AccountBookingHistory';
 import UserProfile from '../components/UserProfile';
 import { Container, Grid } from '@mui/material';
+import axios from 'axios';
 
 const AccountManagement = () => {
     const [activeItem, setActiveItem] = useState('account');
     const [sidebarOpen, setSidebarOpen] = useState(false);
+    const [profile, setProfile] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [currentView, setCurrentView] = useState('profile'); // State to manage the current view
+    const [user, setUser] = useState(null);
+  
+
+    //  Get Token
+    useEffect(() => {
+      const token = localStorage.getItem("auth_token");
+      if (token) {
+        axios
+          .post("http://127.0.0.1:8000/api/decodetoken", { token: token })
+          .then((response) => {
+            setUser(response.data["data"]);
+            console.log("RESPONSE DATA: ", response.data["data"]);
+          })
+          .catch((error) => {
+            alert("Error decoding JWT token:", error);
+            setUser(null);
+          });
+      } else {
+        setUser(null);
+      }
+    }, []);
+
+    // Fetch Profile Data
+    useEffect(() => {
+        const fetchProfile = async () => {
+          if (!user) return; // Exit if user is not set
+          try {
+            const response = await axios.get(`http://127.0.0.1:8000/api/getusers/${user.userid}`);
+            console.log("Response Data:", response.data); // Log the entire response object
+            console.log("Response Data UserID:", response.data.userid); // Log the entire response object
+            setProfile(response.data);
+            setLoading(false);
+          } catch (error) {
+            setError("Error fetching profile data.");
+            console.error(error);
+            setLoading(false);
+          }
+        };
+    
+        fetchProfile();
+      }, [user]); // Add user as a dependency here
+    
 
     const handleItemClick = (item) => {
         setActiveItem(item);
@@ -121,7 +168,7 @@ const AccountManagement = () => {
 
                 {/* Main Content */}
                 <Grid item xs={10} style={{  transition: 'margin-left 0.3s ease' , backgroundColor: ''}}>
-                    {activeItem === 'account' && <UserProfile />}
+                    {activeItem === 'account' && <UserProfile  profile={profile} />}
                     {activeItem === 'bookings' && <BookingHistory />}
                     {/* Add more conditional rendering based on activeItem */}
                 </Grid>
--- fe/src/pages/AccountManagement/components/AccountSignIn.js
+++ fe/src/pages/AccountManagement/components/AccountSignIn.js
@@ -1,23 +1,50 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../css/AccountID.css";
-import { Divider, TextField, InputAdornment, Paper } from "@mui/material";
-// import AccountCircle from "@mui/icons-material/PermIdentity";
-import IconButton from "@mui/material/IconButton";
+import { Divider, TextField, InputAdornment, Paper, IconButton, Typography } from "@mui/material";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
-import Typography from "@mui/material/Typography";
 import Lock from "@mui/icons-material/Lock";
+import axios from "axios";
 
-export default function AccountSignIn() {
+export default function AccountSignIn({ profile }) {
   const [showCurrentPassword, setShowCurrentPassword] = useState(false);
   const [showNewPassword, setShowNewPassword] = useState(false);
   const [showConfirmNewPassword, setShowConfirmNewPassword] = useState(false);
+  const [isChanged, setIsChanged] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [profilePassword, setProfilePassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmNewPassword, setConfirmNewPassword] = useState('');
 
-  const handleToggleShowCurrentPassword = () =>
-    setShowCurrentPassword((prev) => !prev);
+  // Update current password from profile
+  useEffect(() => {
+    if (profile) {
+      setProfilePassword(profile.password);
+    }
+  }, [profile]);
+
+ // Track changes to enable/disable the save button
+ useEffect(() => {
+  setIsChanged(
+    newPassword.length > 0 &&
+    newPassword === confirmNewPassword
+  );
+}, [newPassword, confirmNewPassword,]);
+
+  const handleToggleShowCurrentPassword = () => setShowCurrentPassword((prev) => !prev);
   const handleToggleShowNewPassword = () => setShowNewPassword((prev) => !prev);
-  const handleToggleShowConfirmNewPassword = () =>
-    setShowConfirmNewPassword((prev) => !prev);
+  const handleToggleShowConfirmNewPassword = () => setShowConfirmNewPassword((prev) => !prev);
+
+  const handleSavePassword = async (e) => {
+    
+  };
+
+  const handleCancel = () => {
+    setCurrentPassword(profile.currentPassword);
+    setNewPassword('');
+    setConfirmNewPassword('');
+    setIsChanged(false); // Reset the state after cancelling
+  };
 
   return (
     <Paper className="account-cntr" sx={{ borderRadius: "12px" }}>
@@ -31,7 +58,6 @@ export default function AccountSignIn() {
 
       <Divider orientation="vertical" sx={{ padding: "2rem" }} />
 
-      {/* Current, New, Confirm New Password*/}
       <div className="account-id-cntr">
         <Typography
           variant="h6"
@@ -49,6 +75,8 @@ export default function AccountSignIn() {
           required
           id="current-password"
           label="Current Password"
+          value={currentPassword}
+          onChange={(e) => setCurrentPassword(e.target.value)}
           type={showCurrentPassword ? "text" : "password"}
           sx={{
             "& .MuiOutlinedInput-root": {
@@ -58,7 +86,6 @@ export default function AccountSignIn() {
           InputProps={{
             startAdornment: (
               <InputAdornment position="start" sx={{ marginRight: "1rem" }}>
-                {/* <AccountCircle /> */}
                 <Lock />
               </InputAdornment>
             ),
@@ -80,6 +107,8 @@ export default function AccountSignIn() {
           required
           id="new-password"
           label="New Password"
+          value={newPassword}
+          onChange={(e) => setNewPassword(e.target.value)}
           type={showNewPassword ? "text" : "password"}
           sx={{
             "& .MuiOutlinedInput-root": {
@@ -89,7 +118,6 @@ export default function AccountSignIn() {
           InputProps={{
             startAdornment: (
               <InputAdornment position="start" sx={{ marginRight: "1rem" }}>
-                {/* <AccountCircle /> */}
                 <Lock />
               </InputAdornment>
             ),
@@ -111,6 +139,8 @@ export default function AccountSignIn() {
           required
           id="confirm-new-password"
           label="Confirm New Password"
+          value={confirmNewPassword}
+          onChange={(e) => setConfirmNewPassword(e.target.value)}
           type={showConfirmNewPassword ? "text" : "password"}
           sx={{
             "& .MuiOutlinedInput-root": {
@@ -120,7 +150,6 @@ export default function AccountSignIn() {
           InputProps={{
             startAdornment: (
               <InputAdornment position="start" sx={{ marginRight: "1rem" }}>
-                {/* <AccountCircle /> */}
                 <Lock />
               </InputAdornment>
             ),
@@ -137,9 +166,22 @@ export default function AccountSignIn() {
             ),
           }}
         />
+
         <div className="account-btn-cntr">
-          <button className="save-btn">Save</button>
-          <button className="cancel-btn">Cancel</button>
+          <button
+            className={`save-btn ${isChanged ? 'save-btn-withChanges' : 'save-btn-withoutChanges'}`}
+            onClick={handleSavePassword}
+            disabled={!isChanged}
+          >
+            Save
+          </button>
+          <button
+            className={`cancel-btn ${isChanged ? 'cancel-btn-withChanges' : 'cancel-btn-withoutChanges'}`}
+            onClick={handleCancel}
+            disabled={!isChanged}
+          >
+            Cancel
+          </button>
         </div>
       </div>
     </Paper>
