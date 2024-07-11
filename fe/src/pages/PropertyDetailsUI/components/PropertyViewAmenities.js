f7f6d3547f15ddf1696b4ab8093d426e5430fedb
Kaye <134757409+Kayegdp0802@users.noreply.github.com>
2024-07-11 21:32:27 +0800

    Update the propertyhouserules UI (#44)

--- fe/src/pages/PropertyDetailsUI/components/PropertyViewAmenities.js
+++ fe/src/pages/PropertyDetailsUI/components/PropertyViewAmenities.js
@@ -20,33 +20,7 @@ const amenitiesIcons = {
   Microwave: "microwave.png",
 };
 
-const Amenities = ({ amenities = [] }) => (
-  <Paper className="info-cntr" sx={{ borderRadius: "12px" }}>
-    <div className="info-title-cntr">
-      <ArrowRight sx={{ color: "#16B4DD" }} />
-      <div>Amenities</div>
-    </div>
-    <Divider sx={{ width: "100%", color: "#ccc" }} />
-    <div className="amenity-cntr">
-      {/* Render the list of amenities or a message if no amenities are available */}
-      {amenities.length > 0 ? (
-        amenities.map((amenity) => (
-          <div className="each-amenity" key={amenity.amenity_name}>
-            <img
-              src={amenitiesIcons[amenity.amenity_name]}
-              alt={amenity.amenity_name}
-              style={{ width: "24px", height: "24px", marginRight: "8px" }}
-            />
-            <div className="rooms-name">{amenity.amenity_name}</div>
-          </div>
-        ))
-      ) : (
-        <div className="no-amenities">No Amenities Available</div>
-      )}
-    </div>
-  </Paper>
-);
-
+const Amenities = ({ amenities = [] }) => {
   return (
     <Paper className="info-cntr" sx={{ borderRadius: "12px" }}>
       <div className="info-title-cntr">
@@ -55,17 +29,20 @@ const Amenities = ({ amenities = [] }) => (
       </div>
       <Divider sx={{ width: "100%", color: "#ccc" }} />
       <div className="amenity-cntr">
-        {/* Render the list of amenities */}
-        {amenities.map((amenity) => (
-          <div className="each-amenity" key={amenity.amenity_name}>
-            <img
-              src={amenitiesIcons[amenity.amenity_name]}
-              alt={amenity.amenity_name}
-              style={{ width: "24px", height: "24px", marginRight: "8px" }}
-            />
-            <div className="rooms-name">{amenity.amenity_name}</div>
-          </div>
-        ))}
+        {amenities.length > 0 ? (
+          amenities.map((amenity) => (
+            <div className="each-amenity" key={amenity.amenity_name}>
+              <img
+                src={amenitiesIcons[amenity.amenity_name]}
+                alt={amenity.amenity_name}
+                style={{ width: "24px", height: "24px", marginRight: "8px" }}
+              />
+              <div className="rooms-name">{amenity.amenity_name}</div>
+            </div>
+          ))
+        ) : (
+          <div className="no-amenites">No Amenities Available</div>
+        )}
       </div>
     </Paper>
   );
