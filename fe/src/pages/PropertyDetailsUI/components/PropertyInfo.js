900b777328dddc1919993dd22a72f36a16d5417e
Kaye <kayegdp@gmail.com>
2024-09-09 00:42:26 +0800

    updating the multiunit buttons and icons

--- fe/src/pages/PropertyDetailsUI/components/PropertyInfo.js
+++ fe/src/pages/PropertyDetailsUI/components/PropertyInfo.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import "../css/PropertyInfo.css";
 import RoomDetails from "./RoomDetails";
@@ -11,28 +11,19 @@ import PropertyCancellation from "./Cancellation";
 
 export default function PropertyInfo({ propertyImages, propertyInfo }) {
   const [propertyImg, setPropertyImg] = useState([]);
-  const [propertyDetails, setPropertyDetails] = useState({});
   const [rooms, setTransformedRooms] = useState([]);
-  const [activeSection, setActiveSection] = useState("gallery");
+  const [activeSection, setActiveSection] = useState("");
 
   useEffect(() => {
     if (propertyImages && propertyInfo && propertyInfo.property_unitdetails) {
       setPropertyImg(propertyImages);
-      setPropertyDetails(propertyInfo);
-      console.log("ARI NAA?", propertyInfo);
 
-      // Flattened arrays to hold all rooms and beds
       const allRooms = [];
-      const allBeds = [];
 
       propertyInfo.property_unitdetails.forEach((unit) => {
         const unitrooms = unit.unitrooms || [];
         const unitbeds = unit.unitbeds || [];
 
-        // Create a map to track which bedrooms have been added
-        const bedroomMap = {};
-
-        // Map through unitbeds to create bedroom entries with details
         const detailedRooms = unitbeds.map((bedroom, index) => {
           const bedTypes = Object.entries(bedroom.beds || {}).map(
             ([type, count]) => (
@@ -42,70 +33,96 @@ export default function PropertyInfo({ propertyImages, propertyInfo }) {
             )
           );
           const name = `Bedroom #${index + 1}`;
-          bedroomMap[name] = true;
           return { name, details: bedTypes };
         });
 
-        // Add non-bedroom rooms from unitrooms
         const nonBedroomRooms = unitrooms
           .map((room) => {
             if (room.roomname === "Bedroom") {
-              return null; // Skip bedroom as they are already handled
+              return null;
             }
             return { name: room.roomname };
           })
           .filter(Boolean);
 
-        // Combine detailedRooms and nonBedroomRooms
         allRooms.push(...detailedRooms, ...nonBedroomRooms);
       });
 
-      console.log("COMBINED ROOMS", allRooms);
       setTransformedRooms(allRooms);
     }
   }, [propertyImages, propertyInfo]);
 
+  const isSingleUnit =
+    propertyInfo?.property_unitdetails &&
+    propertyInfo.property_unitdetails.length === 1;
+
+  useEffect(() => {
+    // Set the default section based on the property type
+    if (isSingleUnit) {
+      setActiveSection("gallery");
+    } else {
+      setActiveSection("amenities");
+    }
+  }, [isSingleUnit]);
+
   const renderSection = () => {
-    switch (activeSection) {
-      case "gallery":
-        return <RoomGallery propertyImages={propertyImg} />;
-      case "details":
-        return <RoomDetails rooms={rooms} />;
-      case "amenities":
-        return <PropertyViewAmenities propertyinfo={propertyInfo} />;
-      case "facilities":
-        return <PropertyViewFacilities propertyinfo={propertyInfo} />;
-      case "services":
-        return <PropertyViewServices propertyinfo={propertyInfo} />;
-      case "houseRules":
-        return <PropertyHouseRules propertyinfo={propertyInfo} />;
-      case "cancellation":
-        return <PropertyCancellation propertyinfo={propertyInfo} />;
-      default:
-        return null;
+    if (isSingleUnit) {
+      switch (activeSection) {
+        case "gallery":
+          return <RoomGallery propertyImages={propertyImg} />;
+        case "details":
+          return <RoomDetails rooms={rooms} />;
+        case "amenities":
+          return <PropertyViewAmenities propertyinfo={propertyInfo} />;
+        case "facilities":
+          return <PropertyViewFacilities propertyinfo={propertyInfo} />;
+        case "services":
+          return <PropertyViewServices propertyinfo={propertyInfo} />;
+        case "houseRules":
+          return <PropertyHouseRules propertyinfo={propertyInfo} />;
+        case "cancellation":
+          return <PropertyCancellation propertyinfo={propertyInfo} />;
+        default:
+          return null;
+      }
+    } else {
+      switch (activeSection) {
+        case "amenities":
+          return <PropertyViewAmenities propertyinfo={propertyInfo} />;
+        case "facilities":
+          return <PropertyViewFacilities propertyinfo={propertyInfo} />;
+        case "services":
+          return <PropertyViewServices propertyinfo={propertyInfo} />;
+        case "houseRules":
+          return <PropertyHouseRules propertyinfo={propertyInfo} />;
+        case "cancellation":
+          return <PropertyCancellation propertyinfo={propertyInfo} />;
+        default:
+          return null;
+      }
     }
   };
 
   return (
     <div style={{ marginBottom: "2rem" }}>
       <Box className="sort-menu">
-       <button
-          className="sort-btn"
-        >
-          View As
-        </button>
-        <button
-          className="sort-btn"
-          onClick={() => setActiveSection("gallery")}
-        >
-          Gallery
-        </button>
-        <button
-          className="sort-btn"
-          onClick={() => setActiveSection("details")}
-        >
-          Room Details
-        </button>
+        <button className="sort-btn">View As</button>
+        {isSingleUnit && (
+          <>
+            <button
+              className="sort-btn"
+              onClick={() => setActiveSection("gallery")}
+            >
+              Gallery
+            </button>
+            <button
+              className="sort-btn"
+              onClick={() => setActiveSection("details")}
+            >
+              Room Details
+            </button>
+          </>
+        )}
         <button
           className="sort-btn"
           onClick={() => setActiveSection("amenities")}
--- fe/src/pages/PropertyDetailsUI/components/PropertyViewFacilities.js
+++ fe/src/pages/PropertyDetailsUI/components/PropertyViewFacilities.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import { Typography, Divider } from "@mui/material";
@@ -7,25 +6,26 @@ import Grid from "@mui/material/Grid";
 import { useParams } from "react-router-dom";
 import Container from "@mui/material/Container";
 import ArrowRight from "@mui/icons-material/Send";
+import PoolIcon from "@mui/icons-material/Pool";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import SpaIcon from "@mui/icons-material/Spa";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import SportsSoccerIcon from "@mui/icons-material/SportsSoccer";
+import LocalParkingIcon from "@mui/icons-material/LocalParking";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
 import "../css/PropertyBenefits.css";
 
 const facilitiesIcons = {
-  "Swimming Pool": "swimmingpool.png",
-  Gym: "gym.png",
-  "Wellness Facilities": "wellness.png",
-  "Game Room": "gameroom.png",
-  "Sports Facilities": "sports.png",
-  Parking: "parking.png",
-  "Business Center": "businesscenter.png",
+  "Swimming Pool": <PoolIcon sx={{ color: "#16B4DD" }} />,
+  Gym: <FitnessCenterIcon sx={{ color: "#16B4DD" }} />,
+  "Wellness Facilities": <SpaIcon sx={{ color: "#16B4DD" }} />,
+  "Game Room": <SportsEsportsIcon sx={{ color: "#16B4DD" }} />,
+  "Sports Facilities": <SportsSoccerIcon sx={{ color: "#16B4DD" }} />,
+  Parking: <LocalParkingIcon sx={{ color: "#16B4DD" }} />,
+  "Business Center": <BusinessCenterIcon sx={{ color: "#16B4DD" }} />,
 };
 
 const Facilities = ({ facilities = [] }) => (
-  // useEffect(() => {
-  //   console.log("Facilities:", facilities);
-  // }, [facilities]);
-  // if (facilities.length === 0) {
-  //   return null;
-  // }
   <Paper className="info-cntr" sx={{ borderRadius: "12px" }}>
     <div className="info-title-cntr">
       <ArrowRight sx={{ color: "#16B4DD" }} />
@@ -36,12 +36,10 @@ const Facilities = ({ facilities = [] }) => (
       {facilities.length > 0 ? (
         facilities.map((facility) => (
           <div className="each-amenity" key={facility.facilities_name}>
-            <img
-              src={facilitiesIcons[facility.facilities_name]}
-              alt={facility.facilities_name}
-              style={{ width: "24px", height: "24px", marginRight: "8px" }}
-            />
-            <div className="rooms-name">{facility.facilities_name}</div>
+            {facilitiesIcons[facility.facilities_name]}
+            <div className="rooms-name" style={{ marginLeft: "8px" }}>
+              {facility.facilities_name}
+            </div>
           </div>
         ))
       ) : (
@@ -52,14 +50,13 @@ const Facilities = ({ facilities = [] }) => (
 );
 
 export default function PropertyViewFacilities({ propertyinfo }) {
-  const [loading, setLoading] = useState(true); // Loading state
+  const [loading, setLoading] = useState(true);
   const [propertyInfo, setPropertyInfo] = useState();
 
   useEffect(() => {
     try {
       if (propertyinfo) {
         setPropertyInfo(propertyinfo);
-        // console.log("PROPERTY INFO", propertyinfo);
       }
     } catch (err) {
       console.log(err);
