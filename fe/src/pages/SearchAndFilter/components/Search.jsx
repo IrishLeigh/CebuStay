34b1ce3c2bf04b5b35a375af0232be770b68765d
ludivicoB <117965365+ludivicoB@users.noreply.github.com>
2024-09-21 23:39:06 +0800

    Search Input Minor Changes -click outside results will hide result -click inside the search input will display the results -empty search input will clear results

--- fe/src/pages/SearchAndFilter/components/Search.jsx
+++ fe/src/pages/SearchAndFilter/components/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import '../css/Search.css';
 import { MdSearch } from 'react-icons/md';
 import DatePicker from 'react-datepicker';
@@ -7,17 +7,26 @@ import LocationOnIcon from '@mui/icons-material/LocationOn';
 import BedIcon from '@mui/icons-material/Bed';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-export default function Search({ onSearch, accommodations, setAccommodationList  }) {
+
+export default function Search({ onSearch, accommodations, setAccommodationList }) {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(null);
     const [guestCapacity, setGuestCapacity] = useState('');
     const [query, setQuery] = useState('');
     const [suggestions, setSuggestions] = useState([]);
+    const [showSuggestions, setShowSuggestions] = useState(false);
     const navigate = useNavigate();
+    const suggestionsRef = useRef(null);
+
     const handleSearch = () => {
         onSearch({ startDate, endDate, guestCapacity: guestCapacity || null });
     };
-
+    useEffect(() => {
+        console.log('Query:', query);
+        console.log('Suggestions:', suggestions);
+        console.log('Show Suggestions:', showSuggestions);
+    }, [query, suggestions, showSuggestions]);
+    
     const fetchSuggestions = async (query) => {
         if (!query) {
             setSuggestions([]);
@@ -30,7 +39,6 @@ export default function Search({ onSearch, accommodations, setAccommodationList 
                 ...response.data.properties.map(prop => ({ type: 'Property', name: prop }))
             ];
             setSuggestions(combinedSuggestions);
-            // console.log('Search results:', combinedSuggestions);
         } catch (error) {
             console.error('Error fetching search results:', error);
             setSuggestions([]);
@@ -42,18 +50,28 @@ export default function Search({ onSearch, accommodations, setAccommodationList 
         setQuery(inputValue);
         if (inputValue.length > 0) {
             fetchSuggestions(inputValue);
+            setShowSuggestions(true);
         } else {
-            setSuggestions([]);
+            setSuggestions([]); // Clear suggestions when input is empty
+            setShowSuggestions(false);
+        }
+    };
+
+    const handleClickOutside = (event) => {
+        if (suggestionsRef.current && !suggestionsRef.current.contains(event.target)) {
+            setShowSuggestions(false);
         }
     };
 
     const debouncedFetchResults = useCallback(debounce(fetchSuggestions, 300), []);
+
     useEffect(() => {
-        
         if (query) {
             debouncedFetchResults(query);
+            setShowSuggestions(true);
         } else {
-            setSuggestions([]);
+            setSuggestions([]); // Clear suggestions if the query is empty
+            setShowSuggestions(false);
         }
 
         return () => {
@@ -61,11 +79,18 @@ export default function Search({ onSearch, accommodations, setAccommodationList 
         };
     }, [query]);
 
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     const handleSuggestionClick = (suggestion) => {
         setQuery(suggestion.name.address || suggestion.name.property_name);
-        setSuggestions([]); // Force clearing the suggestions list
-        console.log('Suggestion clicked:', suggestion);
-        if(suggestion.type === 'Location') {
+        setSuggestions([]); // Clear the suggestions list on selection
+        setShowSuggestions(false); // Hide suggestions
+        if (suggestion.type === 'Location') {
             const filteredAccommodations = accommodations.filter(acc => acc.address === suggestion.name.address);
             setAccommodationList(filteredAccommodations);
         } else {
@@ -79,46 +104,46 @@ export default function Search({ onSearch, accommodations, setAccommodationList 
             navigate(`/accommodation/property/${suggestion.name.propertyid}`, { state });
         }
     };
-    
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }}>
             <div className="m" style={{ width: '100%', marginTop: '2rem' }}>
                 <div className="search-box">
-                    <div style={{ display: 'flex', alignItems: 'column',  }}>
-                    <input
-                        type="text"
-                        placeholder="Search Destination"
-                        className="input-field"
-                        id="whereInput"
-                        value={query}
-                        onChange={handleInputChange}
-                    />
-                    
-                    {suggestions.length > 0 && (
-                         <ul className="suggestions-list"  style={{ top: '12rem' }}>
-                         {suggestions.map((suggestion, index) => (
-                             <li
-                                 key={index}
-                                 className="suggestion-item"
-                                 
-                                 onClick={() => handleSuggestionClick(suggestion)}
-                             >
-                                 {suggestion.type === 'Location' ? (
-                                     <LocationOnIcon className="location-icon" />
-                                 ) : (
-                                     <BedIcon className="location-icon" />
-                                 )}
-                                 <div className="suggestion-text">
-                                     {suggestion.name.address || suggestion.name.property_name}
-                                     <span className="suggestion-description">{suggestion.type}</span>
-                                 </div>
-                             </li>
-                         ))}
-                     </ul>
-                    )}
+                    <div style={{ display: 'flex', alignItems: 'column' }}>
+                        <input
+                            type="text"
+                            placeholder="Search Destination"
+                            className="input-field"
+                            id="whereInput"
+                            value={query}
+                            onChange={handleInputChange}
+                            onFocus={() => setShowSuggestions(true)} // Show suggestions on focus
+                        />
+
+                        {/* Only show the suggestions list when there is a non-empty query and showSuggestions is true */}
+                        {showSuggestions && query.length > 0 && suggestions.length > 0 && (
+                            <ul className="suggestions-list" style={{ top: '11rem' }} ref={suggestionsRef}>
+                                {suggestions.map((suggestion, index) => (
+                                    <li
+                                        key={index}
+                                        className="suggestion-item"
+                                        onClick={() => handleSuggestionClick(suggestion)}
+                                    >
+                                        {suggestion.type === 'Location' ? (
+                                            <LocationOnIcon className="location-icon" />
+                                        ) : (
+                                            <BedIcon className="location-icon" />
+                                        )}
+                                        <div className="suggestion-text">
+                                            {suggestion.name.address || suggestion.name.property_name}
+                                            <span className="suggestion-description">{suggestion.type}</span>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
                     </div>
-                    
+
                     <DatePicker
                         selected={startDate}
                         onChange={date => setStartDate(date)}
@@ -160,14 +185,14 @@ export default function Search({ onSearch, accommodations, setAccommodationList 
 // Custom debounce function
 function debounce(func, wait) {
     let timeout;
-  
+
     function debounced(...args) {
         const context = this;
         clearTimeout(timeout);
         timeout = setTimeout(() => func.apply(context, args), wait);
     }
-  
+
     debounced.cancel = () => clearTimeout(timeout);
-  
+
     return debounced;
 }
