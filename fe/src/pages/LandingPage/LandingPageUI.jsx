a30ddbecfa2228485882fddd3f138b7ad9722bc3
ludivicoB <117965365+ludivicoB@users.noreply.github.com>
2024-10-02 22:20:08 +0800

    footer done - with mobile responsiveness (#151)

--- fe/src/pages/LandingPage/LandingPageUI.jsx
+++ fe/src/pages/LandingPage/LandingPageUI.jsx
@@ -3,6 +3,7 @@ import Explore from './components/Explore';
 import Popular from './components/Popular';
 import Hidden from './components/Hidden';
 import Footer from './components/Footer';
+import FooterNew from './components/FooterNew';
 import BasicGrid from './components/Land';
 import InteractiveMap from '../../InteractiveMap/InteractiveMap';
 import LandingCover from './components/LandingCover';
@@ -36,7 +37,7 @@ const LandingPageUI = () => {
         {/* <Popular /> */}
         {/* <Hidden /> */}
         </div>
-        <Footer/>
+        <FooterNew/>
       </div >
       
    
